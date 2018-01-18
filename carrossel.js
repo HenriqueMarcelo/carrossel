760ea0ad3f7c2c511c7418c747545a7e9bc0a5ea
Henrique <henrique@designtec.com.br>
2018-01-18 17:19:33 -0200

    Inseridos circulos indicadores de página

--- carrossel.js
+++ carrossel.js
@@ -8,16 +8,13 @@ var Carrossel = function(opcoes){
   _this.classeEspecificada = opcoes.classe || 'carrossel';
 
   _this.elementosComClasseEspecificada = document.getElementsByClassName(_this.classeEspecificada);
-  if(_this.elementosComClasseEspecificada.length != 1){
-    console.error("Erro ao encontrar classe especificada");
-  }
-  if(_this.elementosComClasseEspecificada.length > 1){
-    console.error("Muitos Elementos com classe especificada. Crie uma instância de Carrossel para cada slider.");
-  }
+
+  validacoes();
 
   _this.carrossel       = _this.elementosComClasseEspecificada[0];
   _this.itens           = _this.carrossel.children[0];
   _this.setas           = _this.carrossel.children[1];
+  _this.indicadores     = _this.carrossel.children[2];
   _this.setaEsquerda    = _this.setas.children[0];
   _this.setaDireita     = _this.setas.children[1];
   _this.numeroDeBanners = _this.itens.childElementCount;
@@ -59,20 +56,45 @@ var Carrossel = function(opcoes){
     }
   }
 
-  _this.setaEsquerda.addEventListener("click", function callBackEsquerda(){
-    pararLoop();
-    _this.anterior();
-    iniciarLoop();
-  }, false);
+  function validacoes(){
+    if(_this.elementosComClasseEspecificada.length != 1){
+      console.error("Erro ao encontrar classe especificada");
+    }
+    if(_this.elementosComClasseEspecificada.length > 1){
+      console.error("Muitos Elementos com classe especificada. Crie uma instância de Carrossel para cada slider.");
+    }
+  }
 
-  _this.setaDireita.addEventListener("click", function callBackDireita(){
-    pararLoop();
-    _this.proxima();
-    iniciarLoop();
-  }, false);
+  function registraEventosSetas(){
+    _this.setaEsquerda.addEventListener("click", function callBackEsquerda(){
+      pararLoop();
+      _this.anterior();
+      iniciarLoop();
+    }, false);
+
+    _this.setaDireita.addEventListener("click", function callBackDireita(){
+      pararLoop();
+      _this.proxima();
+      iniciarLoop();
+    }, false);
+  }
 
   function trocarImg(){
     _this.itens.style.left = "-"+_this.bannerAtual * 100+"vw";
+    indicadoresAtivos = _this.indicadores.getElementsByClassName('ativo');
+    for (i of indicadoresAtivos) {
+      i.setAttribute("class", "");
+    }
+
+    indicadorAtivo = _this.bannerAtual;
+    if (_this.bannerAtual < 0) {
+      indicadorAtivo = _this.numeroDeBanners - 1;
+    }
+    if (_this.bannerAtual >= _this.numeroDeBanners) {
+      indicadorAtivo = 0;
+    }
+
+    _this.indicadores.children[indicadorAtivo].children[0].setAttribute("class", "ativo");
   }
 
   function criaClones(){
@@ -91,6 +113,53 @@ var Carrossel = function(opcoes){
   function pararLoop(){
     window.clearInterval(_this.loop);
   }
+
+  function criaIndicadores(){
+    for (var i = 0; i < _this.numeroDeBanners; i++) {
+      divIndicador = document.createElement('div');
+      aIndicador   = document.createElement('a');
+      divIndicador.appendChild(aIndicador);
+      _this.indicadores.appendChild(divIndicador);
+      aIndicador.dataset.banner = i;
+    }
+    _this.indicadores.children[0].children[0].setAttribute("class", "ativo")
+  }
+
+  function registraEventosIndicadores(){
+    for (var i = 0; i < _this.indicadores.children.length; i++) {
+      indicador = _this.indicadores.children[i].children[0] ;
+      indicador.addEventListener("click", function callBackIndicador( e ){
+        pararLoop();
+
+        if(_this.bannerAtual >= _this.numeroDeBanners){
+          _this.itens.setAttribute("class", "itens notransition");
+          _this.bannerAtual = 0;
+          trocarImg();
+
+          setTimeout(function() {
+            _this.itens.setAttribute("class", "itens");
+            _this.bannerAtual = e.target.dataset.banner;
+            trocarImg();
+            iniciarLoop();
+          }, 0);
+
+        }else{
+          _this.itens.setAttribute("class", "itens");
+          _this.bannerAtual = e.target.dataset.banner;
+          trocarImg();
+          iniciarLoop();
+        }
+
+      }, false);
+
+    }
+  }
+
+  registraEventosSetas();
+
+  criaIndicadores();
+
+  registraEventosIndicadores();
 
   criaClones();
 
